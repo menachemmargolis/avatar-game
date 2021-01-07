79c00cce02f1b67898dcc46e31bdaa282f4bfd9f
github username <github email address>
2021-01-07 10:58:22 -0500

    abstract ifelse fights

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -8,7 +8,7 @@ class Menu
     end
 
     def menu_choice
-      @prompt.select("welcome to avatar fightinh game, please choose an option") do |menu|
+      @prompt.select("welcome to avatar fighting game, please choose an option") do |menu|
         menu.choice "log in", -> {log_in}
         menu.choice "sign up", -> {sign_up}
         menu.choice "exit", -> {exit}
@@ -68,49 +68,107 @@ class Menu
     end
 
     def choose_skill
-        user_skill_points = 0 
+        user1_hp = 100
+        user2_hp = 100
+        # user1_final_hp = user1_hp.clamp(0, 100)
+        # user2_final_hp = user2_hp.clamp(0, 100)
+
+        user1_skill_points = 0
+        user1_energy = 0 
+        user1_heal = 0
         user2_skill_points = 0
+        user2_energy = 0
+        user2_heal = 0 
+        count = 0
         games = Game.last
-        #game_to_update = Game.all.find{|game|game.id == games.id}
         ci = Character.find_by(id: games.character_id)
         ei = Element.find_by(id: ci.element_id)
         ci2 = Character.find_by(id: games.character_id2)
         ei2 = Element.find_by(id: ci2.element_id)
         skills = Element.all_skill
+      while count < 4
+        system 'clear'
         chosen_skills = @prompt.select("choose your skill", ei.skill_1, ei.skill_2, ei.skill_3, ei.skill_4)
         chosen_skills_2 = [ei2.skill_1, ei2.skill_2, ei2.skill_3, ei2.skill_4]
         random_skill = chosen_skills_2.sample
+
         if chosen_skills == ei.skill_1 
-        user_skill_points = 10 
+          user1_skill_points = 20
+          user1_energy += 30 
+          user1_heal = 0
+          user2_hp -= user1_skill_points
         elsif chosen_skills == ei.skill_2
-          user_skill_points = 5
+          user1_skill_points = 5
+          user1_energy += 50 
+          user1_heal = 30
+          user2_hp -= user1_skill_points
+          user1_hp += user1_heal
         elsif chosen_skills == ei.skill_3
-          user_skill_points = 25
-        elsif chosen_skills == ei.skill_4
-          user_skill_points = 40
+          user1_skill_points = 30
+          user1_energy += 10 
+          user1_heal = 10
+          user2_hp -= user1_skill_points
+          user1_hp += user1_heal
+        elsif chosen_skills == ei.skill_4 && user1_energy >= 100
+          user1_skill_points = 90
+          user1_energy = 0 
+          user1_heal = 0
+          user2_hp -= user1_skill_points
         end
-        
+
         if random_skill == ei2.skill_1 
-          user2_skill_points = 10 
+          user2_skill_points = 20
+          user2_energy += 30 
+          user2_heal = 0
+          user1_hp -= user2_skill_points
           elsif random_skill == ei2.skill_2
             user2_skill_points = 5
+            user2_energy += 50 
+            user2_heal = 30
+            user1_hp -= user2_skill_points
+            user2_hp += user2_heal
           elsif random_skill == ei2.skill_3
-            user2_skill_points = 25
-          elsif random_skill == ei2.skill_4
-            user2_skill_points = 40
+            user2_skill_points = 30
+            user2_energy += 10 
+            user2_heal = 10
+            user1_hp -= user2_skill_points
+            user2_hp += user2_heal
+          elsif random_skill == ei2.skill_4 && user2_energy >= 100
+            user2_skill_points = 90
+            user2_energy = 0
+            user2_heal = 0 
+            user1_hp -= user2_skill_points
           end
 
-          if user_skill_points > user2_skill_points 
+          # if user_skill_points > user2_skill_points 
+          #   puts "you win!"
+          #   games.update(result: "win")
+          # elsif user_skill_points == user2_skill_points
+          #   puts "it's a tie!"
+          #   games.update(result: "tie")
+          # else user_skill_points < user2_skill_points
+          #   puts "you lose!"
+          #   games.update(result: "lose")
+          # end
+
+        puts "You chose #{ci.name} to use #{chosen_skills}"
+        puts "#{ci.name} has #{user1_hp} Health, and #{user1_energy} Energy points"
+        puts "The enemy#{ci2.name} used #{random_skill}"
+        puts "#{ci2.name} has #{user2_hp} Health, and #{user2_energy} Energy points"
+
+          count += 1
+          sleep(5.5)
+        end
+          if user1_hp > user2_hp
             puts "you win!"
             games.update(result: "win")
-          elsif user_skill_points == user2_skill_points
+          elsif user1_hp == user2_hp
             puts "it's a tie!"
             games.update(result: "tie")
-          else user_skill_points < user2_skill_points
+          else user1_hp < user2_hp
             puts "you lose!"
             games.update(result: "lose")
           end
-
       end
         
       def game_helper
